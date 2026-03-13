c51f26425f1ab94cc6df585f842adef1dd0207ae
ayahusseini <aya.ahusseini@gmail.com>
2026-03-13 15:40:43 +0000

    reverted

--- site/app/setup_logging.py
+++ site/app/setup_logging.py
@@ -18,7 +18,7 @@ def setup_logging(
     file_handler.setFormatter(formatter)
 
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
+    console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(formatter)
 
     if app:
