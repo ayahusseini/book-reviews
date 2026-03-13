bbfe7c41e49f65862c4516912d34287bd9bf1541
ayahusseini <aya.ahusseini@gmail.com>
2026-03-13 15:40:07 +0000

    changed log level to DEBUG for console

--- site/app/setup_logging.py
+++ site/app/setup_logging.py
@@ -18,7 +18,7 @@ def setup_logging(
     file_handler.setFormatter(formatter)
 
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.WARNING)
+    console_handler.setLevel(logging.DEBUG)
     console_handler.setFormatter(formatter)
 
     if app:
