8ad4076880d2d03ba95927332e3d80ae9c9482be
ayahusseini <aya.ahusseini@gmail.com>
2026-03-15 07:14:27 +0000

    reorganising and simplifying blueprints

--- site/app/blueprints/books.py
+++ site/app/blueprints/books.py
@@ -1,4 +1,14 @@
 """Blueprint for /books"""
 
+from flask import Blueprint
+
+books_bp = Blueprint("books", __name__)
+
+
+@books_bp.route("/books", methods=["GET"])
+def mainbooks():
+    return "hello world"
+
+
 if __name__ == "__main__":
     pass
